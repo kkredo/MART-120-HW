ce430795c5f102dd3a7111c5a64fee0ae458e5c7
Kyle Kredo <kylekredo@gmail.com>
2019-10-27 15:06:09 -0600

    added A9

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(200,152,70);
+  rect(174,190,55,90);
+  rect(180,280,15,90);
+  rect(208,280,15,90);
+  square(153,190,20);
+  square(230,190,20);
+  rect(233,210,15,90);
+  rect(155,210,15,90);
+  triangle(140,130,197,60,260,130);
+
+  point(215,150);
+  point(190,150);
+  line(190,160,210,160);
+  strokeWeight(5);
+  textSize(20);
+  text('Myself',10,30);
+  text('Kyle Kredo',10,60);
+  
+}
+  
